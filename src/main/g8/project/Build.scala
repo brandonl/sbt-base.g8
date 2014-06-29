2360d455091c229382953959c5ed4ed0e997dd41
lbrandon <brandonluth@gmail.com>
2014-06-29 11:29:59 -0700

    moving sbt idea to project based plugin

--- src/main/g8/project/Build.scala
+++ src/main/g8/project/Build.scala
@@ -6,14 +6,12 @@ object BuildSettings {
     Defaults.defaultSettings ++
       Seq( organization := "$organization$",
            version := "$version$",
-           scalaVersion := "2.10.0",
+           scalaVersion := "2.11.1",
            scalacOptions := Seq( "-unchecked", "-deprecation", "-feature" ),
            scalaSource in Compile <<= baseDirectory( _ / "src" / "main" ),
            scalaSource in Test <<= baseDirectory( _ / "src" / "test" ),
-           resolvers ++= Seq(
-                              "Sonatype OSS Releases" at "http://oss.sonatype.org/content/repositories/releases/",
-                              "Sonatype OSS Snapshots" at "http://oss.sonatype.org/content/repositories/snapshots/")
-         )
+           resolvers ++= Seq( "Sonatype OSS Releases" at "http://oss.sonatype.org/content/repositories/releases/",
+                              "Sonatype OSS Snapshots" at "http://oss.sonatype.org/content/repositories/snapshots/"))
 }
 
 object Build extends Build {
@@ -23,36 +21,6 @@ object Build extends Build {
     "$name$",
     file( "." ),
     settings = buildSettings ++ Seq(
-      libraryDependencies := Seq(
-
-        Dependencies.Runtime.ScalaConfig,
-        Dependencies.Runtime.ScalaLogging,
-        Dependencies.Runtime.LogbackCore,
-        Dependencies.Runtime.LogbackClassic,
-        Dependencies.Runtime.Slf4jApi,
-
-        Dependencies.Test.ScalaTest,
-        Dependencies.Test.Mockito
-        ) )
-  )
-
-  object Dependencies {
-
-    object Compile {
-    }
-
-    object Runtime {
-      val ScalaLogging = "com.typesafe" %% "scalalogging-slf4j" % "1.1.+"
-      val ScalaConfig = "com.typesafe" % "config" % "1.0.0"
-      val LogbackCore = "ch.qos.logback" % "logback-core" % "1.0.+"
-      val LogbackClassic = "ch.qos.logback" % "logback-classic" % "1.0.+"
-      val Slf4jApi = "org.slf4j" % "slf4j-api" % "1.6.+"
-    }
-
-    object Test {
-      val Mockito = "org.mockito" % "mockito-all" % "1.9.0" % "test"
-      val ScalaTest = "org.scalatest" %% "scalatest" % "1.9.+" % "test"
-    }
-  }
+      libraryDependencies := Seq()))
 }
 
